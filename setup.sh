52ebcac5b093c94abdebeb663279cb9b4d94031a
AfterGlow <afterglowstudios7@gmail.com>
2023-08-21 06:21:51 +0000

    run on steup

--- setup.sh
+++ setup.sh
@@ -7,14 +7,12 @@ sudo apt-get install git-lfs
 git lfs install
 
 #get pygmalion
-
 if [ ! -d "/workspace/KoboldAI-Client/models/pygmalion-6b" ]; then
     echo "Directory does not exist"
     git clone https://huggingface.co/PygmalionAI/pygmalion-6b /workspace/KoboldAI-Client
 fi
 
-
 #copy post_start.sh to root
 cp post_start.sh /
-
-echo "setup done.  restart the instance"
+echo "setup done. running post_start.sh...."
+cd / && ./post_start.sh &
