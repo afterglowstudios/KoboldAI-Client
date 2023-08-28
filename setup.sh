6e989cd97c4335031cc9b0d9408bcd97748e57fb
AfterGlow <afterglowstudios7@gmail.com>
2023-08-28 04:35:54 +0000

    update dir

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 apt update
 apt install -y nano screen sudo
 
@@ -9,7 +10,7 @@ git lfs install
 #get pygmalion
 if [ ! -d "/workspace/KoboldAI-Client/models/pygmalion-6b" ]; then
     echo "Directory does not exist"
-    git clone https://huggingface.co/PygmalionAI/pygmalion-6b /workspace/KoboldAI-Client
+    git clone https://huggingface.co/PygmalionAI/pygmalion-6b /workspace/KoboldAI-Client/models
 fi
 
 #copy post_start.sh to root
